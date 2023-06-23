409499cca9d4eca35ee588e68eb26fa295477797
Dini Damayanti <dini.gamelab@gmail.com>
2023-06-23 00:24:04 -0700

    230623

---
+++ src/soal1.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ideapad 3 14ITL6
+ */
+public class soal1 {
+    
+}
